60d8c81394fa28ab1b3b711c1532722336c7415b
Ben <ghostykatyt@gmail.com>
2019-07-11 17:03:15 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        while(true) {
+            for (var i = 0; i < 20; i++) {
+                System.out.println("Hey VSauce, Micheal Here.");
+            }
+        }
+        do{
+            for(int i = 0; i <9; i++){
+                System.out.println("Hey there!");
+            }
+        }while(true);
+
+    }
+}
